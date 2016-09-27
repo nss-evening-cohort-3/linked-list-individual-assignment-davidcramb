7bd261925fdfa4caed424c16981b98f34f17052d
David Cramb <david.cramb@gmail.com>
2016-09-26 21:35:40 -0500

    through unit test line 62

--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -15,14 +15,23 @@ namespace SinglyLinkedLists
         private SinglyLinkedListNode next;
         public SinglyLinkedListNode Next
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return next; }
+            set {
+                if(this == value)
+                {
+                    throw new ArgumentException();
+                } else
+                {
+                    next = value;
+                }
+
+            } //same as public string set_Next(string value) { this.next = value;}
         }
 
         private string value;
         public string Value 
         {
-            get { throw new NotImplementedException(); }
+            get { return this.value; }
         }
 
         public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
@@ -39,10 +48,30 @@ namespace SinglyLinkedLists
 
         public SinglyLinkedListNode(string value)
         {
-            throw new NotImplementedException();
-
+            this.value = value;
+            
             // Used by the visualizer:
+
             allNodes.Add(this);
+        }
+
+        public override bool Equals(System.Object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            SinglyLinkedListNode p = obj as SinglyLinkedListNode;
+            if ((System.Object)p == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (value == p.value);
         }
 
         // READ: http://msdn.microsoft.com/en-us/library/system.icomparable.compareto.aspx
@@ -54,6 +83,11 @@ namespace SinglyLinkedLists
         public bool IsLast()
         {
             throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return this.value;
         }
     }
 }
